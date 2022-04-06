49fbc1e5e3da1035e11f15d1be573278ac69e14c
Marek Aufart <maufart@redhat.com>
2022-04-06 14:47:00 +0200

    README update, code cleanup

--- python/README.md
+++ python/README.md
@@ -13,7 +13,7 @@ usage: tackle-mig-1220.py [-h] [steps ...]
 Migrate data from Tackle 1.2 to Tackle 2.
 
 positional arguments:
-  steps       Steps of migration that should be executed (all by default), options: dump transform upload
+  steps       One or more steps of migration that should be executed (dump and upload by default), options: dump upload
 
 options:
   -h, --help  show this help message and exit
@@ -28,4 +28,26 @@ export TACKLE2_URL=https://tackle-konveyor-tackle.apps.cluster.local
 export TACKLE2_TOKEN=axJsbDcxLisdWDWfca...
 ```
 
-Sample migration data directory is [mig-data](mig-data)
+### Sample command output
+
+```
+$ . config-vars && python tackle-mig-1220.py dump
+Starting Tackle 1.2 -> 2 data migration tool
+Dumping Tackle1.2 objects..
+/usr/lib/python3.10/site-packages/urllib3/connectionpool.py:1013: InsecureRequestWarning: Unverified HTTPS request is being made to host 'tackle-mta.apps.mta02.cnv-qe.rhcloud.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+  warnings.warn(
+/usr/lib/python3.10/site-packages/urllib3/connectionpool.py:1013: InsecureRequestWarning: Unverified HTTPS request is being made to host 'tackle-mta.apps.mta02.cnv-qe.rhcloud.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+  warnings.warn(
+/usr/lib/python3.10/site-packages/urllib3/connectionpool.py:1013: InsecureRequestWarning: Unverified HTTPS request is being made to host 'tackle-mta.apps.mta02.cnv-qe.rhcloud.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+  warnings.warn(
+/usr/lib/python3.10/site-packages/urllib3/connectionpool.py:1013: InsecureRequestWarning: Unverified HTTPS request is being made to host 'tackle-mta.apps.mta02.cnv-qe.rhcloud.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+  warnings.warn(
+/usr/lib/python3.10/site-packages/urllib3/connectionpool.py:1013: InsecureRequestWarning: Unverified HTTPS request is being made to host 'tackle-mta.apps.mta02.cnv-qe.rhcloud.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+  warnings.warn(
+/usr/lib/python3.10/site-packages/urllib3/connectionpool.py:1013: InsecureRequestWarning: Unverified HTTPS request is being made to host 'tackle-mta.apps.mta02.cnv-qe.rhcloud.com'. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/1.26.x/advanced-usage.html#ssl-warnings
+  warnings.warn(
+Writing JSON data files into ./mig-data..
+Done.
+```
+
+Sample migration JSON dump files are available in [mig-data directory](mig-data).
--- python/tackle-mig-1220.py
+++ python/tackle-mig-1220.py
@@ -6,15 +6,9 @@ import requests
 
 ###############################################################################
 
-dataDir = "./mig-data"
-#expectedDirs = ["dump", "dump/application-inventory", "dump/controls", "dump/pathfinder", "transformed", "transformed/application-inventory", "transformed/controls", "transformed/pathfinder"]
-#apiObjects = ["application-inventory/application", "controls/stakeholder", "controls/business-service"] #], "pathfinder/assessments/confidence"]
-
-###############################################################################
-
 parser = argparse.ArgumentParser(description='Migrate data from Tackle 1.2 to Tackle 2.')
 parser.add_argument('steps', type=str, nargs='*',
-                    help='Steps of migration that should be executed (all by default), options: dump transform upload')
+                    help='One or more steps of migration that should be executed (dump and upload by default), options: dump  upload')
 args = parser.parse_args()
 
 ###############################################################################
@@ -194,8 +188,9 @@ class Tackle12Import:
     def load(self):
         for t in self.TYPES:
             dictCollection = loadDump(os.path.join(self.dataDir, t + '.json'))
-            for obj in dictCollection:
-                self.add()
+            for dictObj in dictCollection:
+                obj = Tackle2Object(dictObj)
+                self.add(obj)
 
 class Tackle2Object:
     def __init__(self, initAttrs = {}):
@@ -206,54 +201,7 @@ class Tackle2Object:
 
 ###############################################################################
 
-def dumpTackle1():
-    print("Dumping Tackle 1.2 objects")
-    prepareDirs()
-    checkConfig(["TACKLE1_URL", "TACKLE1_TOKEN"])
-    for path in apiObjects:
-        jsonData = apiJSON(os.environ.get('TACKLE1_URL') + "/api/" + path, os.environ.get('TACKLE1_TOKEN'))
-        saveJSON(os.path.join(dataDir, "dump", path), jsonData['_embedded'][path.rsplit('/')[-1]])  # get rid of object type name from jsonData
-
-def transformApplications():
-    print("Transforming Applications")
-    apps1 = loadDump(os.path.join(dataDir, "dump", "application-inventory", "application.json"))
-    apps2 = []
-    for app1 in apps1:
-        # Application object
-        app2 = Application()
-        app2.id = app1['id']
-        app2.name = app1['name']
-        app2.description = app1['description']
-        apps2.append(app2)
-        # Application-related objects - assessment-risk
-        appFilter = dict()
-        appFilter['applicationId'] = app2.id
-        assessmentRisk = apiJSON(os.environ.get('TACKLE1_URL') + "/api/pathfinder/assessments/assessment-risk", os.environ.get('TACKLE1_TOKEN'), appFilter)
-        # tady načítat další související věci a rovnou z nich dělat data pro import
-    saveJSON(os.path.join(dataDir, "transformed", "application-inventory", "application"), apps2)
-    #saveJSON(os.path.join(dataDir, "transformed", "application-inventory", "assessment-risk.json"), assessmentRisk)
-
-def dumpControls():
-    print("Transforming Controls stakeholders")
-    stakeholders1 = loadDump(os.path.join(dataDir, "dump", "controls", "stakeholder.json"))
-    stakeholders2 = []
-    for sh1 in stakeholders1:
-        sh2 = Application()
-        sh2.id = sh1['id']
-        sh2.name = sh1['displayName']
-        sh2.email = sh1['email']
-        stakeholders2.append(sh1)
-        
-    saveJSON(os.path.join(dataDir, "transformed", "controls", "stakeholder.json"), stakeholders2)
-
-
-def uploadTackle2():
-    print("Uploading Applications")
-    apps = loadDump(os.path.join(dataDir, "dump", "application-inventory", "application.json"))
-    for app in apps:
-        apiJSON(os.environ.get('TACKLE2_URL') + "/api/application", os.environ.get('TACKLE2_TOKEN'), app)
-
-###############################################################################
+dataDir = "./mig-data"
 
 print("Starting Tackle 1.2 -> 2 data migration tool")
 
